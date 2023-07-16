6071c1b7463d24287a055faf7e606ddd5a34f12a
AhmedSamymoh <ahmeds.elnozahy@gmail.com>
2023-07-17 00:16:30 +0300

    Standard Types

---
+++ Std_Types.h
@@ -0,0 +1,27 @@
+/*
+ * Std_Types.h
+ *
+ *  Created on: july , 2023
+ *      Author: Ahmed Samy El Nozahy
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+// ************* DATA TYPES DECLARATION *************
+
+typedef unsigned char         uint8_t;
+typedef unsigned short        uint16_t;
+typedef unsigned int          uint32_t;
+typedef unsigned long         uint64_t;
+
+typedef signed char           sint8_t;
+typedef signed short          sint16_t;
+typedef signed int            sint32_t;
+typedef signed long           sint64_t;
+
+
+
+
+
+#endif
